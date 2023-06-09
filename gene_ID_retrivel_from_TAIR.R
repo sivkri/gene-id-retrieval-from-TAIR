35e75e08f610310bb4aaf211deaaf8dde7dd4ae2
Sivakumar Krishnamoorthy <indiangene@gmail.com>
2023-06-09 14:40:24 +0200

    Update gene_ID_retrivel_from_TAIR.R

--- gene_ID_retrivel_from_TAIR.R
+++ gene_ID_retrivel_from_TAIR.R
@@ -23,3 +23,33 @@ write.csv(res2, file = "mkkk18-1.csv", sep = ";", row.names = TRUE)
 result <- rbind(res, res1, res2)
 res3 <- unique(result)
 write.csv(res3, file = "combined.csv", sep = ";", row.names = TRUE)
+
+#alternative method
+
+library(readxl)
+
+# Function to process the data
+process_data <- function(file_path, output_file) {
+  df <- read.delim2(file_path, header = TRUE, sep = '\t')
+  new_df <- data.frame(df)
+  df$names <- row.names(new_df)
+  res <- cbind(df$names, df$Gene.Model.Type, df$Primary.Gene.Symbol)
+  write.csv(res, file = output_file, sep = ";", row.names = TRUE)
+  return(res)
+}
+
+# Process each file and store the results in a list
+file_paths <- c("C:/Users/siva/Desktop/gene_descriptions_abi1.txt",
+                "C:/Users/siva/Desktop/gene_descriptions_17.txt",
+                "C:/Users/siva/Desktop/gene_descriptions_18.txt")
+
+output_files <- c("abi1td.csv", "mkkk17-1.csv", "mkkk18-1.csv")
+
+results <- lapply(1:length(file_paths), function(i) {
+  process_data(file_paths[i], output_files[i])
+})
+
+# Combine the results
+result <- do.call(rbind, results)
+res3 <- unique(result)
+write.csv(res3, file = "combined.csv", sep = ";", row.names = TRUE)
